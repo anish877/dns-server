38cf8bb71c7756605144acdd969631ff52d62613
anish877 <62011810+anish877@users.noreply.github.com>
2024-10-02 21:54:04 +0530

    stage 2 add question section

--- app/main.js
+++ app/main.js
@@ -1,11 +1,5 @@
 const dgram = require("dgram");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
-// Uncomment this block to pass the first stage
-//
-
 function createDNSHeader() {
   const header = Buffer.alloc(12); // DNS header is 12 bytes
 
